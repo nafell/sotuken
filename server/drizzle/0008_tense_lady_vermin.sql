d46833635627a97db8d0d0e173cdf92362d8c52e
nafell <pixel68.43@gmail.com>
2025-12-14 05:41:01 +0900

    fix db migration

--- server/drizzle/0008_tense_lady_vermin.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "server_validated_at" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "req_w2wr_pres" boolean;--> statement-breakpoint
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "req_binding_count_ok" boolean;--> statement-breakpoint
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "req_pattern_match" boolean;--> statement-breakpoint
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "req_stage_forward_rate" real;--> statement-breakpoint
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "js_parse_ok" boolean;--> statement-breakpoint
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "js_policy_ok" boolean;--> statement-breakpoint
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "w2wr_category" text;--> statement-breakpoint
-ALTER TABLE "experiment_trial_logs" ADD COLUMN "l1plus_validated_at" timestamp with time zone;
